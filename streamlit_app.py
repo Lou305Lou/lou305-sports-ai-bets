56d37dd40902a229c375b790a30e21dff32bd06c
Lou305Lou <llanos.luis.305@gmail.com>
2026-03-17 21:10:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Sports AI Betting Dashboard")
+st.write("App is running successfully.")
